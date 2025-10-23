5b843d72f761604a9acd26beb1e16c93baae3ef0
Junior <aj.junior1203@gmail.com>
2025-10-23 17:12:02 -0300

    Adicionados ícones para PWA

--- static/service-worker.js
+++ static/service-worker.js
@@ -2,6 +2,10 @@ self.addEventListener('install', function(event) {
   console.log('Service Worker instalado');
 });
 
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker ativado');
+});
+
 self.addEventListener('fetch', function(event) {
   event.respondWith(fetch(event.request));
 });
